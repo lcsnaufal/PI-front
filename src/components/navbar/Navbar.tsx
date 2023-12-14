a276a49af663e09565ca501d08235bd392311f55
lcsnaufal <lcsqnaufal@gmail.com>
2023-12-13 21:51:41 -0300

    navbar and react dom

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+    return(
+        <div>
+            dwadadawawdawaw
+        </div>
+    )
+}
+
+export default Navbar;
