85af9775d20f5d8d3fb43d8ce754507bc530b563
teste <lcsqnaufal@gmail.com>
2023-12-14 13:58:31 -0300

    model

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,8 +3,10 @@ import "./Navbar.css";
 
 function Navbar() {
     return(
-        <div>
-            dwadadawawdawaw
+        <div className="navbarFather">
+            <text>IMAGEM</text>
+            <text>Mercado Usados</text>
+            <text>ANUNCIAR</text>
         </div>
     )
 }
